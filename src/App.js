5115d7d1464a92ee5988a076a50b424e010b616e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-01 15:32:30 +0900

    readme.md 업데이트 및 props 연습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Genre(props) {
+  return <h2>I Like {props.genre}.</h2>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Music</h1>
+          <Genre genre="house"></Genre>
+          <Genre genre="disco"></Genre>
+          <Genre genre="techno"></Genre>
+          <Genre genre="jazz"></Genre>
+        </div>
       </header>
     </div>
   );
